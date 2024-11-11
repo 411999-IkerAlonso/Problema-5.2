dc63320fea560d691bacf56d8069a029a1b4ab06
411999-IkerAlonso <411999@tecnicatura.frc.utn.edu.ar>
2024-11-11 02:14:37 -0300

    Falta terminar el editar y Eliminar

--- Problema-5.2/Form1.Designer.cs
+++ Problema-5.2/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -294,6 +294,7 @@
             this.btnEditar.TabIndex = 27;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnBorrar
             // 
@@ -322,6 +323,7 @@
             this.btnCancelar.TabIndex = 30;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnSalir
             // 
@@ -335,37 +337,37 @@
             // 
             // dgvAlumnos
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvAlumnos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAlumnos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dgvAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAlumnos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.legajo,
             this.nombre_alumno,
             this.apellido_alumno});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvAlumnos.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAlumnos.DefaultCellStyle = dataGridViewCellStyle5;
             this.dgvAlumnos.Location = new System.Drawing.Point(791, 99);
             this.dgvAlumnos.Name = "dgvAlumnos";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvAlumnos.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAlumnos.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             this.dgvAlumnos.RowHeadersWidth = 51;
             this.dgvAlumnos.RowTemplate.Height = 24;
             this.dgvAlumnos.Size = new System.Drawing.Size(351, 285);
--- Problema-5.2/Form1.cs
+++ Problema-5.2/Form1.cs
@@ -15,7 +15,8 @@ namespace Problema_5._2
     {
         private DAO DAO = new DAO();
         Alumno alumno = new Alumno();
-        List<Parametro> parametros = new List<Parametro>(); 
+        List<Parametro> parametros = new List<Parametro>();
+        int Modo = 0;
         public frmAlumnos()
         {
             InitializeComponent();
@@ -45,8 +46,8 @@ namespace Problema_5._2
             rbtMasc.Checked = false;
             btnCancelar.Enabled = false;
             btnGrabar.Enabled = false;
-            btnBorrar.Enabled = false;
-            btnEditar.Enabled = false;
+            btnBorrar.Enabled = true;
+            btnEditar.Enabled = true;
             CargarLista("SELECT a.legajo, a.nombre, a.apellido FROM alumnos a");
         }
 
@@ -69,44 +70,53 @@ namespace Problema_5._2
 
         private void btnGrabar_Click(object sender, EventArgs e)
         {
-            alumno.Nombre = txtNombre.Text;
-            alumno.Apellido = txtApellido.Text;
-            alumno.FechaNac = dtpFechaNac.Value;
-            if (rbtFem.Checked)
+            if (Modo ==1)
             {
-                alumno.Sexo = "Femenino";
+                alumno.Nombre = txtNombre.Text;
+                alumno.Apellido = txtApellido.Text;
+                alumno.FechaNac = dtpFechaNac.Value;
+                if (rbtFem.Checked)
+                {
+                    alumno.Sexo = "Femenino";
+                }
+                else if (rbtMasc.Checked)
+                {
+                    alumno.Sexo = "Masculino";
+                }
+                alumno.TipoDoc = Convert.ToInt32(cboTipoDoc.SelectedValue);
+                alumno.Doc = Convert.ToInt32(txtDoc.Text);
+                alumno.Calle = Convert.ToString(txtCalle.Text);
+                alumno.NroCalle = Convert.ToInt32(txtNro.Text);
+                if (chbActividad.Checked)
+                {
+                    alumno.Actividad = true;
+                }
+                else alumno.Actividad = false;
+                if (chbCasado.Checked)
+                {
+                    alumno.Casado = true;
+                }
+                else alumno.Casado = false;
+                if (chbHijos.Checked)
+                {
+                    alumno.Hijos = true;
+                }
+                else
+                {
+                    alumno.Hijos = false;
+                    txtCantidad.Enabled = false;
+                };
+                alumno.Carrera = Convert.ToInt32(cboCarrera.SelectedValue);
+                //Si llega a dar error cambiar aquellos valores que en la Bd son int y aca string, por int
+                NuevoAlumno();
+                CargarLista("SELECT a.legajo, a.nombre, a.apellido FROM alumnos a");
             }
-            else if (rbtMasc.Checked)
+            else if(Modo == 2)
             {
-                alumno.Sexo = "Masculino";
+                int legajoAlumno = Convert.ToInt32(dgvAlumnos.CurrentRow.Cells[0].Value); 
+                EditarAlumno(legajoAlumno);
+                CargarLista("SELECT a.legajo, a.nombre, a.apellido FROM alumnos a");
             }
-            alumno.TipoDoc = Convert.ToInt32(cboTipoDoc.SelectedValue);
-            alumno.Doc = Convert.ToInt32(txtDoc.Text);
-            alumno.Calle = Convert.ToString(txtCalle.Text);
-            alumno.NroCalle = Convert.ToInt32(txtNro.Text);
-            if (chbActividad.Checked)
-            {
-                alumno.Actividad = true;
-            }
-            else alumno.Actividad = false;
-            if (chbCasado.Checked)
-            {
-                alumno.Casado = true;
-            }
-            else alumno.Casado = false;
-            if (chbHijos.Checked)
-            {
-                alumno.Hijos = true;
-            }
-            else
-            {
-                alumno.Hijos = false;
-                txtCantidad.Enabled = false;
-            };
-            alumno.Carrera = Convert.ToInt32(cboCarrera.SelectedValue);
-            //Si llega a dar error cambiar aquellos valores que en la Bd son int y aca string, por int
-            NuevoAlumno();
-            CargarLista("SELECT a.legajo, a.nombre, a.apellido FROM alumnos a");
         }
 
         private void NuevoAlumno()
@@ -129,9 +139,48 @@ namespace Problema_5._2
             int filasAfectadas = DAO.Actualizar(consultaSql, parametros);
             if (filasAfectadas == 0)
             {
-                MessageBox.Show("Algo ha salido mal, el alumno no se ejecuto");
+                MessageBox.Show("Algo ha salido mal, el alumno no se insertó");
             }
-            else MessageBox.Show("Alumno Creado con exito");
+            else MessageBox.Show("Alumno creado con exito");
+        }
+         
+        private void EditarAlumno(int legajo)
+        {
+            string consultaSql = "UPDATE Alumnos SET  " +
+                "nombre = @nombre, " +
+                "apellido = @apellido, " +
+                "fecha_nac = @fecha_nac, " +
+                "sexo = @sexo, " +
+                "tipo_doc = @tipo_doc, " +
+                "doc = @doc, " +
+                "calle = @calle, " +
+                "numero = @numero, " +
+                "actividad = @actividad, " +
+                "casado = @casado, " +
+                "hijos = @hijos, " +
+                "cantidad_hijos = @cantidad_hijos, " +
+                "carrera = @carrera " +
+                "WHERE legajo = @legajo";
+            parametros.Add(new Parametro("@nombre", alumno.Nombre));
+            parametros.Add(new Parametro("@apellido", alumno.Apellido));
+            parametros.Add(new Parametro("@fecha_nac", alumno.FechaNac));
+            parametros.Add(new Parametro("@sexo", alumno.Sexo));
+            parametros.Add(new Parametro("@tipo_doc", alumno.TipoDoc));
+            parametros.Add(new Parametro("@doc", alumno.Doc));
+            parametros.Add(new Parametro("@calle", alumno.Calle));
+            parametros.Add(new Parametro("@numero", alumno.NroCalle));
+            parametros.Add(new Parametro("@actividad", alumno.Actividad));
+            parametros.Add(new Parametro("@casado", alumno.Casado));
+            parametros.Add(new Parametro("@hijos", alumno.Hijos));
+            parametros.Add(new Parametro("@cantidad_hijos", alumno.Cantidad));
+            parametros.Add(new Parametro("@carrera", alumno.Carrera));
+            parametros.Add(new Parametro("@legajo", alumno.Legajo));
+            int filasAfectadas = DAO.Actualizar(consultaSql, parametros);
+            if (filasAfectadas == 0)
+            {
+                MessageBox.Show("Algo ha salido mal, el alumno no se edito");
+            }
+            else MessageBox.Show("Alumno editado con exito");
         }
 
         private void CargarLista(string consultaSql)
@@ -145,7 +194,62 @@ namespace Problema_5._2
         }
         private void btnNuevo_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Completa los cambios para poder Agregar un alumno");
             btnGrabar.Enabled = true;
+            btnBorrar .Enabled = false;
+            btnEditar .Enabled = false;
+            btnCancelar .Enabled = true;
+            Modo = 1;
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            int legajoAlumno = Convert.ToInt32(dgvAlumnos.Rows[0]);
+            CargarAlumno(legajoAlumno);
+            btnNuevo.Enabled = false;
+            btnBorrar .Enabled = false;
+            btnGrabar .Enabled = true;
+            Modo = 2;
+            btnCancelar .Enabled = true;
+        }
+
+        private void Cancelar()
+        {
+            CargarCboCarrera();
+            CargarCboTipoDoc();
+            cboTipoDoc.SelectedIndex = -1;
+            cboCarrera.SelectedIndex = -1;
+            rbtFem.Checked = true;
+            rbtMasc.Checked = false;
+            btnCancelar.Enabled = false;
+            btnGrabar.Enabled = false;
+            btnBorrar.Enabled = true;
+            btnEditar.Enabled = true;
+            CargarLista("SELECT a.legajo, a.nombre, a.apellido FROM alumnos a");
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCalle.Clear();
+            txtCantidad.Clear();
+            txtDoc.Clear();
+            txtNro.Clear();
+            dtpFechaNac.Value= DateTime.Now;
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Cancelar();
+        }
+
+        private void CargarAlumno(int legajo)
+        {
+            string consultaSql = $"SELECT * FROM alumnos WHERE legajo = {legajo}";
+            DataTable dt = DAO.ConsultarBD(consultaSql);
+            if(dt.Rows.Count > 0)
+            {
+                DataRow dr = dt.Rows[0];
+                alumno.Legajo = Convert.ToInt32(dr[0]);
+                alumno.Nombre = Convert.ToString(dr[1]);
+                alumno.Apellido = Convert.ToString(dr[2]);
+            }
         }
     }
 }
